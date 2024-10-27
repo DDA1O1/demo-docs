792781cd38cd58216f3df90c2caf6d421913c70a
debashish <debashishdash03412@gmail.com>
2024-10-27 21:43:57 +0530

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,37 +2,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			logo: {
-				src: './src/assets/9.png',
-				// replacesTitle: true,
-			},
-			social: {
-				github: 'https://github.com/DDA1O1',
-				twitter: 'https://twitter.com/withastro',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/DDA101/docs/',
-			  },
+  integrations: [
+      starlight({
+          title: 'My Docs',
+          logo: {
+              src: './src/assets/9.png',
+              // replacesTitle: true,
+          },
+          social: {
+              github: 'https://github.com/DDA1O1',
+              twitter: 'https://twitter.com/withastro',
+          },
+          editLink: {
+              baseUrl: 'https://github.com/DDA101/docs/',
+            },
 
-			// Configure the sidebar.
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+          // Configure the sidebar.
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  output: 'server',
+  adapter: vercel({
+	webAnalytics: { enabled: true }
+  }),
 });
