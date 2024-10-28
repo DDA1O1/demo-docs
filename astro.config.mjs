c6ac4d30c51f87240fae6a7371c8e44926786a9f
debashish <debashishdash03412@gmail.com>
2024-10-28 19:26:36 +0530

    sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,6 +38,10 @@ export default defineConfig({
                   label: 'Reference',
                   autogenerate: { directory: 'reference' },
               },
+              {
+                  label: 'code blocks',
+                  autogenerate: { directory: 'code blocks' },
+              },
           ],
       }),
 	],
